24b52c8076189f6ceb70f7077668c0b58c90643a
ktertikas <ktertikas@gmail.com>
2016-10-30 11:55:29 +0000

    Initial Commit, Home Controller Added

---
+++ WebDV/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDV.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Hello Ahmet!!";
+
+        }
+    }
+}
