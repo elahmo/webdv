9979b8bc10555a5905216664cdd40db7b7732500
ktertikas <ktertikas@gmail.com>
2016-10-30 13:32:55 +0000

    Updated User class for DB use

--- WebDV/Models/User.cs
+++ WebDV/Models/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace WebDV.Models
 {
@@ -14,4 +15,8 @@ namespace WebDV.Models
         public string email { get; set; }
         public string password { get; set; }
     }
+    public class UserContext : DbContext
+    {
+        public UserContext() : base() { }
+        public DbSet<User> UserDB { get; set; }
 }
