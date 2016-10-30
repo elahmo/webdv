c24f9026bd96e5bb473812a5bd08749540fa2825
ktertikas <ktertikas@gmail.com>
2016-10-30 13:04:33 +0000

    Edited user class in Models

--- WebDV/Models/User.cs
+++ WebDV/Models/User.cs
@@ -7,5 +7,11 @@ namespace WebDV.Models
 {
 	public class User
 	{
-	}
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int role { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
 }
