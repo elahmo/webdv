5f61c9c8276111d023d4b249ea3773bbbc10b727
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-10-30 13:04:28 +0000

    Added fields for Submission model

--- WebDV/Models/Submission.cs
+++ WebDV/Models/Submission.cs
@@ -7,5 +7,11 @@ namespace WebDV.Models
 {
     public class Submission
     {
+        public int submissionID { get; set; }
+        public int userID { get; set; }
+        public DateTime date { get; set; }
+        public int Grade { get; set; }
+        public string feedbackText { get; set; }
+        public int feedbackAuthor { get; set; }
     }
 }
