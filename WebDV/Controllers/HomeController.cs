85d8ff814a88115229027a2431838a14aef098a5
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-10-30 13:37:11 +0000

    Updated homecontroller to speak with database

--- WebDV/Controllers/HomeController.cs
+++ WebDV/Controllers/HomeController.cs
@@ -4,25 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebDV.Models;
+using System.Data.Entity;
 
 namespace WebDV.Controllers
 {
     public class HomeController : Controller
     {
-        private Submission[] Submissions =
+        private SubmissionContext SContext;
+        private DbSet<Submission> SubmissionDB;
+        private Submission[] Submissions;
+        public HomeController()
         {
-            new Submission { submissionID = 0, date = new DateTime(2016, 1, 5), feedbackAuthor = 0, feedbackText = "Good Job", grade = 10, userID = 1},
-            new Submission { submissionID = 1, date = new DateTime(2016, 2, 4), feedbackAuthor = 0, feedbackText = "Hola Amigo como estas?", grade = 9, userID = 1},
-            new Submission { submissionID = 2, date = new DateTime(2016, 3, 3), feedbackAuthor = 0, feedbackText = "Well that's fine.", grade = 8, userID = 1},
-            new Submission { submissionID = 3, date = new DateTime(2016, 4, 2), feedbackAuthor = 0, feedbackText = "Hooray!!", grade = 6, userID = 1},
-            new Submission { submissionID = 4, date = new DateTime(2016, 5, 1), feedbackAuthor = 0, feedbackText = "Money!!!", grade = 5, userID = 1}
-        };
+            SContext = new Models.SubmissionContext();
+            SubmissionDB = SContext.SubmissionDB;
+            SubmissionDB.Add(new Submission { submissionID = 0, date = new DateTime(2016, 1, 5), feedbackAuthor = 0, feedbackText = "Good Job", grade = 10, userID = 1 });
+            SubmissionDB.Add(new Submission { submissionID = 1, date = new DateTime(2016, 2, 4), feedbackAuthor = 0, feedbackText = "Hola Amigo como estas?", grade = 9, userID = 1 });
+            SubmissionDB.Add(new Submission { submissionID = 2, date = new DateTime(2016, 3, 3), feedbackAuthor = 0, feedbackText = "Well that's fine.", grade = 8, userID = 1 });
+            SubmissionDB.Add(new Submission { submissionID = 3, date = new DateTime(2016, 4, 2), feedbackAuthor = 0, feedbackText = "Hooray!!", grade = 6, userID = 1 });
+            SubmissionDB.Add(new Submission { submissionID = 4, date = new DateTime(2016, 5, 1), feedbackAuthor = 0, feedbackText = "Money!!!", grade = 5, userID = 1 });
+            SContext.SaveChanges();
+            Submissions = SubmissionDB.ToArray();
+        }
 
         [Route("/")]
         [HttpGet]
         public ViewResult Index()
         {
-            
+
             return View(Submissions);
 
         }
