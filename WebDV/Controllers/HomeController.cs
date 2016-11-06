e004c9f45633da019844dadb19d4dc31d7be7c12
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-11-06 15:26:47 +0000

    Clean home controller, wont add new entries since we can write to DB directly now

--- WebDV/Controllers/HomeController.cs
+++ WebDV/Controllers/HomeController.cs
@@ -17,12 +17,6 @@ namespace WebDV.Controllers
         {
             SContext = new Models.SubmissionContext();
             SubmissionDB = SContext.SubmissionDB;
-            SubmissionDB.Add(new Submission { submissionID = 0, date = new DateTime(2016, 1, 5), feedbackAuthor = 0, feedbackText = "Good Job", grade = 10, userID = 1 });
-            SubmissionDB.Add(new Submission { submissionID = 1, date = new DateTime(2016, 2, 4), feedbackAuthor = 0, feedbackText = "Hola Amigo como estas?", grade = 9, userID = 1 });
-            SubmissionDB.Add(new Submission { submissionID = 2, date = new DateTime(2016, 3, 3), feedbackAuthor = 0, feedbackText = "Well that's fine.", grade = 8, userID = 1 });
-            SubmissionDB.Add(new Submission { submissionID = 3, date = new DateTime(2016, 4, 2), feedbackAuthor = 0, feedbackText = "Hooray!!", grade = 6, userID = 1 });
-            SubmissionDB.Add(new Submission { submissionID = 4, date = new DateTime(2016, 5, 1), feedbackAuthor = 0, feedbackText = "Money!!!", grade = 5, userID = 1 });
-            SContext.SaveChanges();
             Submissions = SubmissionDB.ToArray();
         }
 
