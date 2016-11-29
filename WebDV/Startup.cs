b6103801e17853ac34ca2ee6e9c7aeed8d50545d
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-11-29 14:57:58 +0000

    Added initial roles creation if they are not present in the database!

--- WebDV/Startup.cs
+++ WebDV/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using WebDV.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebDV.Startup))]
 namespace WebDV
@@ -9,6 +12,25 @@ namespace WebDV
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            setUpRoles();
+        }
+        public void setUpRoles()
+        {
+            ApplicationDbContext appContext = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(appContext));
+
+            if (!roleManager.RoleExists("Student"))
+            {
+                var studentRole = new IdentityRole();
+                studentRole.Name = "Student";
+                roleManager.Create(studentRole);
+            }
+            if (!roleManager.RoleExists("Lecturer"))
+            {
+                var lecturerRole = new IdentityRole();
+                lecturerRole.Name = "Lecturer";
+                roleManager.Create(lecturerRole);
+            }
         }
     }
 }
