9a9c55919fece4663c439ffdd4aa815fb791cc67
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-12-02 12:48:58 +0000

    Added some constraints

--- WebDV/Models/Submission.cs
+++ WebDV/Models/Submission.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebDV.Models
 {
     public class Submission
     {
+        [Key,Required(ErrorMessage = "submissionID is required")]
         public int submissionID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "userID is required")]
         public string userID { get; set; }
+        [Required(ErrorMessage = "date is required")]
         public DateTime date { get; set; }
-        [Required]
+        [Required(ErrorMessage = "submissionData is required")]
         public byte[] submissionData { get; set; }
+        [Required, Range(-1, 10,ErrorMessage = "Grade must be between -1 or 10.")]
         public int grade { get; set; }
-        [StringLength(200)]
+        [StringLength(200, ErrorMessage = "Maximum length of feedback text is 200 characters.")]
         public string feedbackText { get; set; }
         public string feedbackAuthor { get; set; }
     }
